6a9d003ad69989494a524eccbd406143aab06706
Can Nasıf <can.nasif@v3rii.com>
2026-06-30 23:54:12 +0300

    Align Netsis read controller with CRM pattern

--- Modules/NetsisIntegrations/Api/NetsisReadController.cs
+++ Modules/NetsisIntegrations/Api/NetsisReadController.cs
@@ -1,18 +1,26 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using salesdesk_api.Modules.AccessControl.Api;
+using salesdesk_api.Modules.AccessControl.Application.Services;
 using salesdesk_api.Modules.NetsisIntegrations.Application.Dtos;
 using salesdesk_api.Modules.NetsisIntegrations.Application.Services;
 using salesdesk_api.Shared.Common.Application;
+using salesdesk_api.Shared.Infrastructure.Services.Localization;
 
 namespace salesdesk_api.Modules.NetsisIntegrations.Api;
 
 [ApiController]
 [Route("api/[controller]")]
-public sealed class NetsisReadController : ControllerBase
+[Authorize]
+public sealed class NetsisReadController : PermissionProtectedControllerBase
 {
     private readonly INetsisReadService _netsisReadService;
 
-    public NetsisReadController(INetsisReadService netsisReadService)
+    public NetsisReadController(
+        INetsisReadService netsisReadService,
+        IPermissionAccessService permissionAccessService,
+        ILocalizationService localizationService)
+        : base(permissionAccessService, localizationService)
     {
         _netsisReadService = netsisReadService;
     }
